5607efa2145addeab166d091180bc259df8d4696
Leevi Lehtonen <lekale20@hotmail.com>
2014-06-02 20:22:04 +0300

    PollMachine

---
+++ PollMachine/src/com/pollmachine/main/Main.java
@@ -0,0 +1,14 @@
+package com.pollmachine.main;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PollMachine machine = new PollMachine();
+		SwingUtilities.invokeLater(machine);
+
+	}
+
+}
---
+++ PollMachine/src/com/pollmachine/main/PollMachine.java
@@ -0,0 +1,21 @@
+package com.pollmachine.main;
+
+import javax.swing.JFrame;
+
+public class PollMachine implements Runnable{
+	
+	private JFrame frame;
+	
+	public PollMachine() {
+		
+		frame = new JFrame();
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
